6c6dd2fb905f651747e99e1db35d3c2b9a551f3f
coffeequ <savvapugach2004@mail.ru>
2025-03-04 18:53:09 +0500

    settings for electron builder

--- electron/main.js
+++ electron/main.js
@@ -1,14 +1,27 @@
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
 
-const createWindow = () =>{
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-    });
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
 
-    win.loadURL("https://pagtion.vercel.app/");
+  win.loadFile('https://pagtion.vercel.app/login');
 }
 
-app.on("ready", () => {
-    createWindow();
-});
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
