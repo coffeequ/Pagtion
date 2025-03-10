5ce447d23865c311cf54ca4723b49e4b214ef6b8
coffeequ <savvapugach2004@mail.ru>
2025-03-10 13:08:02 +0500

    edit mail send site

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendPasswordResetEmail = async(email: string, token: string) => {
-    const resetLink = `http://localhost:3000/new-password?token=${token}`;
+    const resetLink = `http://pagtion-vercel-app/new-password?token=${token}`;
 
     await resend.emails.send({
         from: "onboarding@resend.dev",
@@ -18,7 +18,7 @@ export const sendPasswordResetEmail = async(email: string, token: string) => {
 }
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-    const confirmLink = `http://localhost:3000/new-verification?token=${token}`;
+    const confirmLink = `http://pagtion-vercel-app/new-verification?token=${token}`;
 
     await resend.emails.send({
         from: "onboarding@resend.dev",
